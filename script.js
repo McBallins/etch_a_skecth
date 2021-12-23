720521a32234dde511c15d2756adebd3ec2b7aea
McBallins <warier@live.com>
2021-12-22 20:49:37 -0600

    index.html

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const newGridBtn = document.getElementById('newgrid');
+newGridBtn.addEventListener
+
 const master = document.querySelector('.master');
 
 function makeGrid(n) {
@@ -5,15 +8,28 @@ function makeGrid(n) {
   let masterWidth = master.clientWidth/n;
   const gridSize = n;
   for (r=0; gridSize>r; r++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.setAttribute('style', `display: flex; height: ${masterWidth}px`);
+    const newRow = document.createElement('div');
+    newRow.classList.add('row');
+    newRow.setAttribute('style', `display: flex; height: ${masterWidth}px`);
     for (s=0; gridSize>s; s++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      square.setAttribute('style', `border: 1px solid black; margin; 0 -1 0 -1 box-sizing: border-box; width: ${masterWidth}px`);
-      row.append(square);
+      const newSquare = document.createElement('div');
+      newSquare.classList.add('square');
+      newSquare.setAttribute('style', `border: 1px solid black; box-sizing: border-box; width: ${masterWidth}px`);
+      newRow.append(newSquare);
     }
-    master.append(row);
+    master.append(newRow);
   }
+  addHoverEffect();
+}
+
+function addHoverEffect() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener("mouseover", function() {
+      square.style.backgroundColor = 'red';
+    });
+    square.addEventListener("mouseout", function() {
+      square.style.backgroundColor = 'white';
+    });
+  })
 }
