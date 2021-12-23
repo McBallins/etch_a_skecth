aba343d99fd40784038df27512b112dcf2ed14c9
McBallins <warier@live.com>
2021-12-22 21:22:12 -0600

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const newGridBtn = document.getElementById('newgrid');
-newGridBtn.addEventListener
-
 const master = document.querySelector('.master');
 
-function makeGrid(n) {
-  master.innerHTML = ''
-  let masterWidth = master.clientWidth/n;
-  const gridSize = n;
+newGridBtn.addEventListener('click', function makeNewGrid() {
+  master.innerHTML = '';
+  userSelection = prompt('How many squares would you like in the grid (1-100)x(1-100)?');
+  if(userSelection > 100 || userSelection < 1) {
+    master.textContent = "Must be a number between 1 and 100!";
+  } else {
+    makeGrid(userSelection);
+  }
+});
+
+function makeGrid(userSelection) {
+  const masterWidth = master.clientWidth/userSelection;
+  const gridSize = Math.ceil(userSelection);
   for (r=0; gridSize>r; r++) {
     const newRow = document.createElement('div');
     newRow.classList.add('row');
